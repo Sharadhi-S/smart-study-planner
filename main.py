5a6b3ffc4ba08cc79decb5bd2a6a281a61957201
Sharadhi-S <sharadhisreeram@gmail.com>
2026-06-17 12:57:53 +0530

    Implemented task viewing

--- main.py
+++ main.py
@@ -10,10 +10,26 @@ def main_menu():
     choice = input("Please enter your choice (1-6): ")
     return choice
 
+tasks = []
+
 while True:
     choice = main_menu()
-    print(f"You selected option {choice}.")
-    if choice == '6':
+    if choice == '1':
+        task = input("Enter the task description: ")
+        if task=="":
+            print("Task description cannot be empty.")
+            continue
+        else:
+            tasks.append({"Name": task, "Status": "Pending"})
+            print(f"Task '{task}' added.")
+    elif choice == '2':
+        if not tasks:
+            print("No tasks available.")
+        else:
+            print("Tasks:")
+            for idx, task in enumerate(tasks, start=1):
+                print(f"{idx}. {task['Name']} - {task['Status']}")
+    elif choice == '6':
         print("Exiting the Smart Study Planner. Goodbye!")
         break
     else:
