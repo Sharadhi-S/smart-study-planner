04cb52f1225579a6f7b4706203bfde21caeae536
Sharadhi-S <sharadhisreeram@gmail.com>
2026-06-17 14:07:00 +0530

    Implemented task deletion

--- main.py
+++ main.py
@@ -14,14 +14,18 @@ tasks = []
 
 while True:
     choice = main_menu()
+    
+    # Adding tasks
     if choice == '1':
-        task = input("Enter the task description: ")
+        task = input("Enter the task description: ").title()
         if task=="":
             print("Task description cannot be empty.")
             continue
         else:
             tasks.append({"Name": task, "Status": "Pending"})
             print(f"Task '{task}' added.")
+    
+    # Viewing tasks
     elif choice == '2':
         if not tasks:
             print("No tasks available.")
@@ -29,6 +33,8 @@ while True:
             print("Tasks:")
             for idx, task in enumerate(tasks, start=1):
                 print(f"{idx}. {task['Name']} - {task['Status']}")
+    
+    # Marking tasks as completed
     elif choice == '3':
         if not tasks:
             print("No tasks available to complete.")
@@ -39,8 +45,29 @@ while True:
                 print(f"Task '{tasks[int(task_num) - 1]['Name']}' marked as completed.")
             else:
                 print("Invalid task number.")
+    
+    # Deleting tasks
+    elif choice == '4':
+        if not tasks:
+            print("No tasks available to delete.")
+        else:
+            task_num = input("Enter the task number to delete: ")
+            confirm = input("Are you sure you want to delete this task? (y/n): ")
+            if confirm.lower() == 'n':
+                print("Task deletion cancelled.")
+            else:
+                if task_num.isdigit() and 1 <= int(task_num) <= len(tasks):
+                    removed_task = tasks.pop(int(task_num) - 1)
+                    print(f"Task '{removed_task['Name']}' deleted.")
+                else:
+                    print("Invalid task number.")
+
+    # Saving tasks to a file
+
+    # Exiting the program            
     elif choice == '6':
         print("Exiting the Smart Study Planner. Goodbye!")
         break
     else:
+        print("Invalid choice. Please try again.")
         continue
